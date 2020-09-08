306e7e604f3c5185908d99ee68e28fe2ec4c080a
Scott Bellware <sbellware@gmail.com>
2020-09-07 19:26:29 -0500

    The gem installation script is updated to new standard version

--- source/install-gems.sh
+++ source/install-gems.sh
@@ -1,23 +1,94 @@
 #!/usr/bin/env bash
 
-echo "Removing gems directory..."
-rm -rf gems
-echo "Removing .bundle directory..."
-rm -rf .bundle
-echo "Removing Gemfile.lock file..."
-rm -f Gemfile.lock
+set -euo pipefail
+
+function boolean-var {
+  variable_name=$1
+  default=${2:-no}
+
+  val=${!variable_name:=$default}
+
+  if [[ "n|no|f|false|off|0" =~ $val ]]; then
+    echo 'false'
+  elif [[ "y|yes|t|true|on|1" =~ $val ]]; then
+    echo 'true'
+  else
+    echo "Variable \$$variable_name is set to \`$val' which is not a boolean value" >&2
+    echo >&2
+    exit 1
+  fi
+}
+
+if [ -z ${REMOVE_GEMS+x} ]; then
+  echo
+  echo "REMOVE_GEMS is not set. Using \"on\" by default."
+  remove_gems="on"
+else
+  remove_gems=$REMOVE_GEMS
+fi
+
+gem_dir="./gems"
+
+echo
+echo "Install Gems"
+echo "= = ="
+
+if [ -z ${POSTURE+x} ]; then
+  echo "POSTURE is not set. Using \"operational\" by default."
+  posture="operational"
+else
+  posture=$POSTURE
+fi
+
+echo "Posture: $posture"
+echo "Gem Directory: $gem_dir"
+echo "Remove Gems: $remove_gems"
+
 echo
 
-set -e
+echo "Removing bundler configuration"
+echo "- - -"
 
-echo "Installing gems to ./gems"
-echo '= = ='
-
-cmd="bundle install --standalone --path=./gems"
+cmd="rm -rfv ./.bundle"
 
 echo $cmd
 ($cmd)
 
-echo '- - -'
-echo '(done)'
+echo
+echo "Removing Gemfile.lock"
+echo "- - -"
+
+cmd="rm -fv Gemfile.lock"
+
+echo $cmd
+($cmd)
+
+remove_gems=$(boolean-var remove_gems)
+
+if $remove_gems; then
+  echo
+  echo "Removing installed gems"
+  echo "- - -"
+
+  cmd="rm -rf $gem_dir"
+
+  echo $cmd
+  ($cmd)
+fi
+
+echo
+echo "Installing bundle"
+echo "- - -"
+
+cmd="bundle install --standalone --path=./gems"
+
+if [ operational == "$posture" ]; then
+  cmd="$cmd --without=development"
+fi
+
+echo $cmd
+($cmd)
+
+echo "- - -"
+echo "(done)"
 echo
